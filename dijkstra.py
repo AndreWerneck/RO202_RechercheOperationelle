af2e855dbf0253228a8f113927f3e65a4a12084c
Andre Werneck <andrecw97@gmail.com>
2023-02-02 02:17:40 +0100

    kruskal and dijkstra algorithms

--- dijkstra.py
+++ dijkstra.py
@@ -1,5 +1,6 @@
 import graph
 import sys
+import numpy as np
 
 def main():
     cities = []
@@ -23,6 +24,7 @@ def main():
     g.addArc("Londres",  "Edimbourg", 2)
     g.addArc("Amsterdam",  "Hambourg", 2)
     g.addArc("Amsterdam",  "Oslo", 8)
+    g.addArc("Amsterdam", "Londres", 1)
     g.addArc("Stockholm",  "Oslo", 2)
     g.addArc("Stockholm",  "Rana", 5)
     g.addArc("Berlin",  "Amsterdam", 2)
@@ -34,20 +36,22 @@ def main():
     g.addArc("Oslo",  "Rana", 2)
     
     # Applique l'algorithme de Dijkstra pour obtenir une arborescence
-    tree = dijkstra(g, "Paris")
-    print(tree)
+    pi,pred = dijkstra(g, "Paris")
+    print('last state of pi = ',pi,'\n','Path from from value to index (exemple:from city 3(value) to city 2(index)  = ',pred)
 
 def dijkstra(g, origin):
 		
    # Get the index of the origin 
    r = g.indexOf(origin)
+   all_arcs = g.getArcs() # get all arcs of the graph
 
    # Next node considered 
    pivot = r
    
    # Liste qui contiendra les sommets ayant été considérés comme pivot
    v2 = []
-   v2.append(r)
+   # v2.append(pivot)
+
    
    pred = [0] * g.n
    
@@ -55,8 +59,40 @@ def dijkstra(g, origin):
    pi = [sys.float_info.max] * g.n
    pi[r] = 0
 
-   # Ajouter votre code ici 
-   # ...
+   # Ajouter votre code ici
+   # look all the nodes of the graph
+   # iter_dict = {}
+   for j in range(1,g.n):
+
+       if pivot not in v2:
+           arcs_pivot = [x for x in all_arcs if x.id1==pivot] # selecting the nodes that have interface with the current pivot
+           arcs_pivot.sort() #sort the distances between the nodes to find the minimal one
+           for i_node in arcs_pivot:
+               if (i_node.weight + pi[pivot]<=pi[i_node.id2]): # if the weight is less than the older one, we update it
+                   if pi[i_node.id2] == sys.float_info.max:  # if the weight is still in infinite and the node is selectec, set the weight to 0
+                       pi[i_node.id2] = 0
+                   pi[i_node.id2] = i_node.weight + pi[pivot] # filling the pi table for the current pivot
+                   pred[i_node.id2] = pivot
+                   # if i_node.id2 != pivot:
+                   #     pred[i_node.id2] = pivot
+
+           v2.append(pivot) # append the older pivot
+           aux=[]
+           for i_v in range(0,g.n):
+               if i_v not in v2:
+                   aux.append(pi[i_v])
+           pivot = pi.index(min(aux)) # update the pivot with the minimum value of pi
+
+           i_i = 0
+           while pivot in v2:  # pivot is a repeated number (as 6 for our first example)
+
+               indexes = np.where(np.array(pi)==pi[pivot])
+               aux2 = indexes[0].tolist().copy()
+               pivot = aux2[i_i] # chooses the pivot that is not repeated
+               i_i=i_i+1
+
+   return pi,pred
+
 
    
 if __name__ == '__main__':
--- kruskal.py
+++ kruskal.py
@@ -5,49 +5,100 @@ import sys
 def main():
     
     # Créer un graphe contenant les sommets a, b, c, d, e, f, g 
-    g = graph.Graph(np.array(["a", "b", "c", "d", "e", "f", "g"]))
+    # exo 3.3
+    # g = graph.Graph(np.array(["a", "b", "c", "d", "e", "f", "g"]))
 
+    # Créer un graphe contenant les sommets a, b, c, d, e, f, g ,h
+    # exo 3.4 - graph 1
+    # g = graph.Graph(np.array(["a", "b", "c", "d", "e", "f", "g","h"]))
+
+    # exo 3.4 - graph
+    g = graph.Graph(np.array(["a", "b", "c", "d", "e", "f"]))
+
+    # #graph exercice 3.3
+    # # Ajouter les arêtes
+    # g.addEdge("a", "b",  1.0)
+    # g.addEdge("a", "c",  8.0) # changed weight for 8, as the firs ex of the TD
+    # g.addEdge("b", "c",  2.0)
+    # g.addEdge("b", "d",  5.0)
+    # g.addEdge("b", "e",  7.0)
+    # g.addEdge("b", "f",  9.0)
+    # g.addEdge("c", "d",  4.0)
+    # g.addEdge("d", "e",  6.0)
+    # g.addEdge("d", "g", 12.0)
+    # g.addEdge("e", "f",  8.0)
+    # g.addEdge("e", "g", 11.0)
+    # g.addEdge("f", "g", 10.0)
+
+    # graph 1 exercice 3.4
     # Ajouter les arêtes
-    g.addEdge("a", "b",  1.0)
-    g.addEdge("a", "c",  3.0)
-    g.addEdge("b", "c",  2.0)
-    g.addEdge("b", "d",  5.0)
-    g.addEdge("b", "e",  7.0)
-    g.addEdge("b", "f",  9.0)
-    g.addEdge("c", "d",  4.0)
-    g.addEdge("d", "e",  6.0)
-    g.addEdge("d", "g", 12.0)
-    g.addEdge("e", "f",  8.0)
-    g.addEdge("e", "g", 11.0)
-    g.addEdge("f", "g", 10.0)
-    
-    # Obtenir un arbre couvrant de poids minimal du graphe
-    tree = kruskal(g)
+    # g.addEdge("a", "b", 9.0)
+    # g.addEdge("a", "f", 6.0)
+    # g.addEdge("a", "h", 9.0)
+    # g.addEdge("b", "c", 5.0)
+    # g.addEdge("b", "d", 8.0)
+    # g.addEdge("b", "e", 5.0)
+    # g.addEdge("c", "d", 2.0)
+    # g.addEdge("c", "g", 5.0)
+    # g.addEdge("d", "h", 7.0)
+    # g.addEdge("d", "g", 8.0)
+    # g.addEdge("e", "f", 1.0)
+    # g.addEdge("e", "g", 3.0)
+    # g.addEdge("g", "h", 5.0)
+
+    # graph 2 exercice 3.4
+    # Ajouter les arêtes
+    g.addEdge("a", "b", 4.0)
+    g.addEdge("a", "c", 3.0)
+    g.addEdge("b", "c", 5.0)
+    g.addEdge("b", "f", 2.0)
+    g.addEdge("c", "d", 2.0)
+    g.addEdge("c", "f", 5.0)
+    g.addEdge("d", "e", 4.0)
+    g.addEdge("d", "f", 3.0)
+    g.addEdge("e", "f", 3.0)
+
+    # Obtenir un arbre couvrant de poids minimal du graphe et le poids minimal
+    tree,cost = kruskal(g,computeMin=False)
     
     # S'il existe un tel arbre (i.e., si le graphe est connexe)
     if tree != None:
         
         # L'afficher
-        print(tree)
+        print(tree,'\n',cost)
     
     else:
         print("Pas d'arbre couvrant")
 
 # Applique l'algorithme de Kruskal pour trouver un arbre couvrant de poids minimal d'un graphe
 # Retourne: Un arbre couvrant de poids minimal du graphe ou None s'il n'en existe pas
-def kruskal(g):
+def kruskal(g,computeMin:bool = True):
     
     # Créer un nouveau graphe contenant les mêmes sommets que g
     tree = graph.Graph(g.nodes)
     
     # Nombre d'arêtes dans l'arbre
     addedEdges = 0
-    
+    cost = 0 # make the calcul of the cost of the whole tree
+
     # Récupérer toutes les arêtes de g
     edges = g.getEdges()
     
     # Trier les arêtes par poids croissant
-    edges.sort()
+    edges.sort(reverse=(not computeMin)) # if computeMin is deactivated, we find the tree with the maximum weight
+
+    # try to detect a circle within the other edges. If not found, at the edge with the minimum weight to the tree.
+    for i_edge in edges:
+        cycle_detected = tree.createACycle(i_edge) #add the first node to the tree -> has the minimum weight then we start with him.
+        if (cycle_detected == False):
+            tree.addCopyOfEdge(i_edge)
+            addedEdges = addedEdges + 1
+            cost = cost + i_edge.weight
+
+    return tree,cost
+
+
+
 
 if __name__ == '__main__':
     main()
